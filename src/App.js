b541fd9bf391791ec7bebe0bad401aef4b1a3e44
alex <gromvolk97@gmail.com>
2022-02-04 23:26:52 +0300

    feat(init project): stage 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import {v4 as uuidv4} from 'uuid'
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('')
+    const [messageList, setMessageList] = useState([])
+
+    useEffect(() => {
+        if (messageList.length === 0) return
+        if (messageList.at(-1).author === 'bot') return
+        setTimeout(() => {
+            const newMessage = {id: uuidv4(), author: 'bot', message: 'i am BOOOT'}
+            setMessageList([...messageList, newMessage])
+        }, 1000)
+    }, [messageList])
+
+    const sendMessage = (value) => {
+        const newMessage = {id: uuidv4(), author: 'user', message: value}
+        setMessageList([...messageList, newMessage])
+    }
+
+    const MessagesItems = () => messageList.map((el) =>
+        <div key={el.id}
+             className={`${'block_message'} ${el.author === 'user' ? 'block_message_user' : 'block_message_bot'}`}>
+            <h4 className={'block_message_author'}>{el.author}</h4>
+            <p className={'block_message_message'}>{el.message}</p>
+        </div>
+    )
+
+    return (
+        <div className="App">
+            <div className={'owner_block_message'}>
+                <div className={'wrapper_block_message'}>
+                    <MessagesItems/>
+                </div>
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    sendMessage(value)
+                }} className={'form'}>
+                    <textarea rows="5" cols="33" placeholder='Write message'
+                              onChange={(e) => setValue(e.target.value)}/>
+                    <button type='submit'>
+                        Send message
+                    </button>
+                </form>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
