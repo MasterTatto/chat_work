365cf2fe96ded469dfc25189ddf2ed740f45a4fd
alex <gromvolk97@gmail.com>
2022-02-06 15:11:52 +0300

    fix(stage3): fix route

--- src/components/chats/index.js
+++ src/components/chats/index.js
@@ -3,22 +3,38 @@ import s from './style.module.css'
 import {v4 as uuidv4} from 'uuid'
 import {List, ListItem} from "@mui/material";
 
+import {NavLink} from "react-router-dom";
+import ChatMessage from "../chatMessages";
+
 const Chats = () => {
-    const [chats, setChats] = useState([{id: uuidv4(), name: 'Bob'}])
+    const [chats, setChats] = useState([
+        {id: uuidv4(), name: 'Bob'},
+        {id: uuidv4(), name: 'Alex'},
+        {id: uuidv4(), name: 'Andrey'},
+        {id: uuidv4(), name: 'Oleg'},
+    ])
+    const [getID, setGetID] = useState()
+
     const ChatItems = () => chats.map((el) =>
-        <ListItem alignItems="flex-start" key={el.id} className={s.chat_item}>
-            <h2>{el.name}</h2>
-        </ListItem>
+        <NavLink to={`/chats/${el.id}`}>
+            <ListItem alignItems="flex-start" key={el.id} className={`${s.chat_item} ${getID === el.id && s.active}`}
+                      onClick={() => setGetID(el.id)}>
+
+                <h2>{el.name}</h2>
+
+            </ListItem>
+        </NavLink>
     )
 
     return (
         <div className={s.chats}>
             <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
-                <ChatItems/>
-            </List>
 
+                <ChatItems/>
+
+            </List>
         </div>
-    );
+    )
 };
 
 export default Chats;
